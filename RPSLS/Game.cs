f3f0e1c1c284d5a58b254042a2db54971d3c219c
alynielson <nielsonaly@gmail.com>
2018-07-24 11:18:16 -0500

    initial classes with initial member variables

---
+++ RPSLS/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Game
+    {
+        public bool isHumanVsComputer;
+        public int numberOfRounds;
+        public int currentRound;
+        Player playerOne;
+        Player playerTwo;
+
+        public Game()
+        {
+           if (isHumanVsComputer == true)
+            {
+                playerTwo = new Computer();
+            }
+           else if (isHumanVsComputer == false)
+            {
+                playerTwo = new Human();
+            }
+        }
+    }
+}
